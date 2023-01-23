058d194ad1128fb2ca77a1cbbbf3a34babc54dcf
robsonwaves <robin.cohenselmon@protonmail.com>
2023-01-23 18:07:46 +0100

    passing to french time, factorizing code

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 import pandas as pd
 from pathlib import Path
 import json
+import pytz
 
 # Press Maj+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
@@ -15,22 +16,19 @@ if __name__ == '__main__':
 
     # Get a list of files in the directory
     for file in files:
+
+        with open(file, 'r') as f:
+            data = json.load(f)
+            df = pd.json_normalize(data, record_path=file.name.split(' ')[0])
+            df['horodatage'] = pd.to_datetime(df['horodatage'], unit='s', utc=True)
+            df['horodatage'] = df.apply(lambda row: row['horodatage'].astimezone(pytz.timezone('Europe/Paris')).strftime(
+                '%Y-%m-%d %H:%M:%S %Z%z'), axis=1)
+
+
+            df = df.set_index('horodatage')
         if 'cnx' not in file.name:
-            with open(file, 'r') as f:
-                data = json.load(f)
-                df = pd.json_normalize(data, record_path=file.name.split(' ')[0])
-                df['horodatage'] = pd.to_datetime(df['horodatage'], unit='s', utc=True)
-                df = df.set_index('horodatage')
-            csv = df.to_csv(file.name.split(' ')[0] + "_" + file.name.split(' ')[1] + "_UTC.csv", sep=';')
+            csv = df.to_csv(file.name.split(' ')[0] + "_" + file.name.split(' ')[1] + "_CET.csv", sep=';')
         else:
-            with open(file, 'r') as f:
-                data = json.load(f)
-                df = pd.json_normalize(data, record_path=file.name.split(' ')[0])
-                df['horodatage'] = pd.to_datetime(df['horodatage'], unit='s', utc=True)
-            csv = df.to_csv(file.name.split(' ')[0] + "_" + 'cnx' + "_UTC.csv", sep=';')
-
-    # Read the JSON file
-
-
+            csv = df.to_csv(file.name.split(' ')[0] + "_" + 'cnx' + "_CET.csv", sep=';')
 
 # See PyCharm help t https://www.jetbrains.com/help/pycharm/
