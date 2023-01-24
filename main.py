d9ef98c8ea9d34b3f52633f42153df8fcca5c7a3
robsonwaves <robin.cohenselmon@protonmail.com>
2023-01-24 22:07:01 +0100

    adding influxdb support

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ import pandas as pd
 from pathlib import Path
 import json
 import pytz
+from influxdb_client import InfluxDBClient, Point, WriteOptions
+from influxdb_client.client.write_api import SYNCHRONOUS
+client = InfluxDBClient(url="http://localhost:8086", token="token_influx", org="my-organisation")
 
 # Press Maj+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
@@ -13,6 +16,8 @@ if __name__ == '__main__':
 
     directory = Path("data")
     files = list(directory.glob("*.txt"))
+    write_api = client.write_api(write_options=WriteOptions(batch_size=50_000, flush_interval=10_000))
+
 
     # Get a list of files in the directory
     for file in files:
@@ -21,12 +26,17 @@ if __name__ == '__main__':
             data = json.load(f)
             df = pd.json_normalize(data, record_path=file.name.split(' ')[0])
             df['horodatage'] = pd.to_datetime(df['horodatage'], unit='s', utc=True)
+            df_for_influx = df
             df['horodatage'] = df.apply(lambda row: row['horodatage'].astimezone(pytz.timezone('Europe/Paris')).strftime(
                 '%Y-%m-%d %H:%M:%S %Z%z'), axis=1)
-
-
+            df_for_influx = df_for_influx.set_index('horodatage')
             df = df.set_index('horodatage')
-        if 'cnx' not in file.name:
+
+        if 'e1' in file.name or 'e2' in file.name:
+            write_api = client.write_api(write_options=WriteOptions(batch_size=50_000, flush_interval=10_000))
+
+            write_api.write("my-bucket", record=df_for_influx, data_frame_measurement_name=file.name.split(' ')[0])
+            write_api.close()
             csv = df.to_csv(file.name.split(' ')[0] + "_" + file.name.split(' ')[1] + "_CET.csv", sep=';')
         elif 'cnx':
             csv = df.to_csv(file.name.split(' ')[0] + "_" + 'cnx' + "_CET.csv", sep=';')
