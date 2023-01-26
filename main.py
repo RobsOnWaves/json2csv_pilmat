d9871a1407bc4b92cdefc26e1301aa8ede2cc15c
robsonwaves <robin.cohenselmon@protonmail.com>
2023-01-26 11:42:08 +0100

    Adding date of insertion as a tag and making as an option to fix SIRSE time

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # This is a sample Python script.
 import pandas as pd
+import datetime
 from pathlib import Path
 import json
 import pytz
@@ -13,7 +14,12 @@ if __name__ == '__main__':
     files = list(directory.glob("*.txt"))
     dts_ids = []
     error_on_influx = False
+    fix_sirse_date = False
+    offset_timestamp = 3600 if fix_sirse_date else 0
+    date_of_insertion = datetime.datetime.now().strftime('%Y_%m_%d_%H_%M_%S')
     # Get a list of files in the directory
+
+
     for file in files:
         if file.name.split(' ')[0] not  in dts_ids:
             dts_ids.append(file.name.split(' ')[0])
@@ -21,21 +27,24 @@ if __name__ == '__main__':
             data = json.load(f)
             df = pd.json_normalize(data, record_path=file.name.split(' ')[0])
             df_for_influx = pd.json_normalize(data, record_path=file.name.split(' ')[0])
-            df['horodatage'] = df['horodatage'] + 3600
-            df_for_influx['horodatage'] = df_for_influx['horodatage'] + 3600
+            df['horodatage'] = df['horodatage'] + offset_timestamp
+            df_for_influx['horodatage'] = df_for_influx['horodatage'] + offset_timestamp
             df['horodatage'] = pd.to_datetime(df['horodatage'], unit='s', utc=True)
             df_for_influx['horodatage'] = pd.to_datetime(df_for_influx['horodatage'], unit='s', utc=True)
+            df_for_influx['date_of_insertion'] = date_of_insertion
             df['horodatage'] = df.apply(lambda row: row['horodatage'].astimezone(pytz.timezone('Europe/Paris')).strftime(
                 '%Y-%m-%d %H:%M:%S %Z%z'), axis=1)
             df = df.set_index('horodatage')
+            df_for_influx = df_for_influx.set_index('horodatage')
 
             try:
                 # Write data to InfluxDB
                 with InfluxDBClient(url="http://localhost:8086", token="token_influx", org="your-organisation") as client:
-                    client.write_api(write_options=SYNCHRONOUS).write(bucket='your-bucket', record=df_for_influx,
-                                                                        data_frame_measurement_name=file.name.split(' ')[0],
-                                                                        data_frame_timestamp_column='horodatage')
 
+                    client.write_api(write_options=SYNCHRONOUS).write(  bucket='your-bucket', record=df_for_influx,
+                                                                        data_frame_measurement_name=file.name.split(' ')[0],
+                                                                        data_frame_tag_columns=['date_of_insertion'])
+                #clientdf.write_points(df_for_influx, 'demo', tag_columns='date_of_insertion')
             except Exception as e:
                 print('No connection to InfluxDB')
                 error_on_influx = True
@@ -56,7 +65,7 @@ if __name__ == '__main__':
                 for dts_id in dts_ids:
                     query = 'from(bucket: "your-bucket")\
                             |> range(start: 2020-01-01, stop: 2023-02-01)\
-                            |> filter(fn: (r) => r["_measurement"] == "' + dts_id + '")\
+                            |> filter(fn: (r) => r["_measurement"] == "' + dts_id + '" and r.date_of_insertion == "' + date_of_insertion + '")\
                             |> filter(fn: (r) => r["_field"] == "e1" or r["_field"] == "e2")\
                             |> pivot(rowKey:["_time"], columnKey: ["_field"], valueColumn: "_value")'
 
